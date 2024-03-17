70ff649cdfab0b0037bc95420b9edd5b7df571fa
cervantesjuand <juancervantes.1389@gmail.com>
2024-03-16 21:15:38 -0500

    cambios

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,38 +2,136 @@ import React from 'react'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter} from "@nextui-org/react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
 import {Input} from "@nextui-org/react";
 import {Select, SelectItem} from "@nextui-org/react";
 
 
 
-export const Tables = () => {
+export const Tables = ({useElementos, UseTipo, listarElementos}) => {
 
-    const [useElementos, setElementos] = useState([]);
+    const [ values, setValues] = useState([]);
+    const [ useActualizar, setActualizar] = useState([]);
+    const [useDesactivar, setDesactivar] = useState([]);
+
+    const BuscarElemento = async (Codigo_elemento) => {
+        try {
+            await axios.get(`http://localhost:3000/elemento/buscar/${Codigo_elemento}`)
+                .then(response => {
+                    setValues(response.data.Elemento)
+                    console.log(response.data.Elemento)
+                    console.log(UseTipo);
+                })
+                
+                
+        } catch {
     
+        }
+      }
 
+      const editarValores=(event) => {
+        setValues({
+            ...values,
+            [event.target.name]:event.target.value
+        });
+        console.log(values);
+    }
 
-    const listarElementos = async () => {
-        await axios.get('http://localhost:3000/elemento/listar')
-            .then(response => {
-                setElementos(response.data)
-                console.log(response.data)
-            })
-    };
-
+    const Actualizar = async (event, Codigo_elemento) => {
+        event.preventDefault();
+        try {
+            const respuesta = await axios.put(`http://localhost:3000/elemento/actualizar/${Codigo_elemento}`, value);
+            if (respuesta.status === 200) {
+                alert(respuesta.data.message);
+            }
+        } catch (error){ 
+            console.log(error)
+        }
+    }
     
+    const desactivarElementos = async (Codigo_elemento) => {
+    await axios.put(`http://localhost:3000/elemento/desactivar/${Codigo_elemento}`)
+        .then(response => {
+            setDesactivar(response.data)
+            listarElementos();
+        })
+  };
 
-    useEffect(() => {
-        listarElementos()
-    }, [])
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const size = '3xl'; // Establece el tamaño del modal como '3xl'
 
-
-    
+    const handleOpen = (Codigo_elemento) => {
+        onOpen();
+        BuscarElemento(Codigo_elemento);
+    }
+ 
     return (
         <div className=''>  
-         
+            <Modal 
+          size={size} 
+          isOpen={isOpen} 
+          onClose={onClose} 
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1 text-[22px]">Información Elemento</ModalHeader>
+                <ModalBody>
+                  <form  action="">
+                    <div className='flex flex-col gap-4'>
+                        <div className='flex gap-5'>
+                          <Input
+                            isRequired
+                            type="text"
+                            label="Nombre del elemento"
+                            value={values.Nombre_elemento}
+                            className="max-w-xs"
+                          />
+                          <Input
+                            isRequired
+                            type="number"
+                            label="Cantidad del elemento"
+                            value={values.stock}
+                            className="max-w-xs"
+                          />
+                        </div>
+                        <Select
+                          label="Tipo Elemento"
+                          placeholder="Selecciona un tipo"
+                          name='fk_tipoElemento'
+                          selectionMode="multiple"
+                          className="max-w-xs"
+                          defaultSelectedKeys={[values.fk_tipoElemento]}
+                           
+                        >
+                          {
+                            UseTipo.map(tipo => ( 
+                              <SelectItem
+                              key={tipo.codigo_Tipo} 
+                              value={tipo.codigo_Tipo}
+                              >
+                                {tipo.nombre_tipoElemento}
+                              </SelectItem>
+                            ))
+                          }
+                        </Select>
+                    </div>
+                    <div className='w-full mt-5 flex justify-end gap-5'>
+                    <Button style={{width: '100px'}} color="danger"  onPress={onClose}>
+                    Close
+                    </Button>
+                    <Button style={{width: '100px'}} color="primary" type='submit' >
+                      Actualizar
+                    </Button>
+                    </div>
+                  </form>
+                </ModalBody>
+                <ModalFooter>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
             <Table removeWrapper aria-label="Example static collection table">
                 <TableHeader style={{}}>
                     <TableColumn  style={{fontSize: '16px', backgroundColor: '#39A900', color: '#fff', textAlign: 'center' }}>Código</TableColumn>
@@ -59,10 +157,10 @@ export const Tables = () => {
                             <TableCell style={{fontSize: '15px'}}>{elemento.Nombre_Medida}</TableCell>
                             <TableCell style={{fontSize: '15px'}}>{elemento.Nombre_ubicacion}</TableCell>
                             <TableCell style={{ display: 'flex', gap: '5px' }}>
-                            <Button color="danger" style={{fontSize: '15px'}}>
+                            <Button color="danger" onClick={()=> {desactivarElementos(elemento.Codigo_elemento)}} style={{fontSize: '15px'}}>
                                  Desactivar
                             </Button>  
-                            <Button color="primary" style={{fontSize: '15px'}}>
+                            <Button color="primary" style={{fontSize: '15px'}} onClick={()=> {handleOpen(elemento.Codigo_elemento)}}>
                                 Información
                             </Button> 
                             </TableCell>
--- src/view/Elemento.jsx
+++ src/view/Elemento.jsx
@@ -1,17 +1,129 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Tables } from '../components/Table'
 import { Modals } from '../components/Modals'
+import axios from 'axios'
 
 
 export const Elemento = () => {
+
+  const [useElementos, setElementos] = useState([]);
+  const [useDesactivar, setDesactivar] = useState([]);
+  const [UseTipo, SetTipo] = useState([]);
+  const [UseCategorias, setCategorias] = useState([]);
+  const [UseUbicacion, SetUbicacion] = useState([]);
+  const [UseEmpaques, SetEmpaques] = useState([]);
+  const [UseMedidas, SetMedidas] = useState([]);
+    
+    
+  const listarElementos = async () => {
+      await axios.get('http://localhost:3000/elemento/listar')
+          .then(response => {
+              setElementos(response.data)
+          })
+  };
+
+
+  const ListarTipo = async () => {
+    try {
+        await axios.get('http://localhost:3000/tipo/listar')
+            .then(response => {
+                SetTipo(response.data)
+            })
+    } catch {
+
+    }
+  }
+
+  const ListarCategorias = async () => {
+    try {
+        await axios.get('http://localhost:3000/categoria/listar')
+            .then(response => {
+                setCategorias(response.data)
+                
+            })
+    } catch {
+
+    }
+  }
+
+  const Listarubicacion = async () => {
+    try {
+        await axios.get('http://localhost:3000/ubicacion/listar')
+            .then(response => {
+                SetUbicacion(response.data)
+                
+            })
+            
+            
+    } catch {
+
+    }
+  }
+
+  const ListarEmpaques = async () => {
+    try {
+        await axios.get('http://localhost:3000/empaque/listar')
+            .then(response => {
+                SetEmpaques(response.data)
+               
+            })
+            
+            
+    } catch {
+
+    }
+  }
+  
+  const ListarMedidas = async () => {
+    try {
+        await axios.get('http://localhost:3000/medida/listar')
+            .then(response => {
+                SetMedidas(response.data)
+                
+            })
+            
+            
+    } catch {
+
+    }
+  }
+  
+
+  useEffect(() => {
+      listarElementos()
+      ListarTipo()
+      ListarCategorias()
+      Listarubicacion()
+      ListarEmpaques()
+      ListarMedidas()
+      
+  }, [])
+
   
   return (
     <div className='w-90% flex flex-col justify-center items-center gap-5'>
       <div className='mt-[50px]'>
         <div>
-          <Modals/>
+        <Modals
+        listarElementos={listarElementos}
+        UseTipo={UseTipo}
+        UseCategorias={UseCategorias}
+        UseUbicacion={UseUbicacion}
+        UseEmpaques={UseEmpaques}
+        UseMedidas={UseMedidas}
+        useDesactivar={useDesactivar}
+        
+          />
         </div>
-        <Tables/>
+        <Tables
+        listarElementos={listarElementos}
+        useElementos={useElementos}
+        UseTipo={UseTipo}
+        UseCategorias={UseCategorias}
+        UseUbicacion={UseUbicacion}
+        UseEmpaques={UseEmpaques}
+        UseMedidas={UseMedidas}
+        useDesactivar={useDesactivar}/>
         </div>
     </div>
  )
