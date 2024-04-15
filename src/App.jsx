4e163d16f1e8e422d0e91e4aad459a4abf062243
sircagev <cf.giron04@gmail.com>
2024-04-15 09:09:29 -0500

    Logout On

--- src/App.jsx
+++ src/App.jsx
@@ -34,40 +34,45 @@ function App() {
    }, []);
 
    return (
-      <>
-         {!loggedIn && <Login setLoggedIn={setLoggedIn} />}
+      <Routes>
+         <Route path="/login" element={<Login setLoggedIn={setLoggedIn}/>} />
 
-         {
-            loggedIn && (
-               <main className={`grid grid-cols-[1fr] ${sidebarOpen ? "tablet:grid-cols-[220px_1fr]" : "tablet:grid-cols-[65px_1fr]"}`}>
-                  <section className="hidden tablet:initial bg-gray-100">
-                     <Sidebar state={sidebarOpen} setState={() => setSidebarOpen(!sidebarOpen)} />
-                  </section>
-                  <section className="flex flex-col w-full h-screen">
-                     <Navbar />
-                     <div className="flex-1 overflow-y-auto bg-gray-100">
-                        <Routes>
-                           <Route path="/" element={<div>Hola</div>} />
-                           <Route path="/elementos" element={<Elementos />} />
-                           <Route path="/elementos/categorias" element={<Categorias />} />
-                           <Route path="/elementos/empaques" element={<Empaque />} />
-                           <Route path="/elementos/medidas" element={<Medida />} />
-                           <Route path="/usuarios" element={<Usuario />} />
-                           <Route path="/reportes/usuarios" element={<ReporteU />} />
-                           <Route path="/reportes/elementos" element={<ReporteE />} />
-                           <Route path="/reportes/bodegas" element={<ReporteB />} />
-                           <Route path="/reportes/movimientos" element={<ReporteM />} />
-                           <Route path="/bodegas" element={<Bodega />} />
-                           <Route path="/reportes" element={<Reporte />} />
-                           <Route path="/bodegas/ubicacion" element={<Ubicacion />} />
-                           <Route path="/movimientos" element={<Movimientos2 />} />
-                        </Routes>
-                     </div>
-                  </section>
-               </main >
-            )
-         }
-      </>
+         <Route
+            path="/*"
+            element={
+               loggedIn ? (
+                  <main className={`grid grid-cols-[1fr] ${sidebarOpen ? "tablet:grid-cols-[220px_1fr]" : "tablet:grid-cols-[65px_1fr]"}`}>
+                     <section className="hidden tablet:initial bg-gray-100">
+                        <Sidebar state={sidebarOpen} setState={() => setSidebarOpen(!sidebarOpen)} />
+                     </section>
+                     <section className="flex flex-col w-full h-screen">
+                        <Navbar setLogIn={setLoggedIn} />
+                        <div className="flex-1 overflow-y-auto bg-gray-100">
+                           <Routes>
+                              <Route path="/" element={<div>Hola</div>} />
+                              <Route path="/elementos" element={<Elementos />} />
+                              <Route path="/elementos/categorias" element={<Categorias />} />
+                              <Route path="/elementos/empaques" element={<Empaque />} />
+                              <Route path="/elementos/medidas" element={<Medida />} />
+                              <Route path="/usuarios" element={<Usuario />} />
+                              <Route path="/reportes/usuarios" element={<ReporteU />} />
+                              <Route path="/reportes/elementos" element={<ReporteE />} />
+                              <Route path="/reportes/bodegas" element={<ReporteB />} />
+                              <Route path="/reportes/movimientos" element={<ReporteM />} />
+                              <Route path="/bodegas" element={<Bodega />} />
+                              <Route path="/reportes" element={<Reporte />} />
+                              <Route path="/bodegas/ubicacion" element={<Ubicacion />} />
+                              <Route path="/movimientos" element={<Movimientos2 />} />
+                           </Routes>
+                        </div>
+                     </section>
+                  </main>
+               ) : (
+                  <Navigate to="/login" />
+               )
+            }
+         />
+      </Routes>
    );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,36 @@ import React from 'react'
 import logo from '../assets/logoInout.png'
 import { FaBell, FaUserCircle } from "react-icons/fa";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { IoMdLogOut } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 
+export const Navbar = ({ setLogIn }) => {
 
-export const Navbar = () => {
-  return (
-    <div className='w-full flex items-center justify-between h-[100px] bg-[#D9DADF text-white'>
-      <div className='flex items-center gap-4'>
-        <img src={logo} className='w-[60px] h-auto ml-10' alt="" />
-        <h1 className='text-black font-bold text-lg'>Inventario de bodegas</h1>
+   const navigate = useNavigate();
+
+   const handleLogout = () => {
+      // Eliminar el token del localStorage
+      localStorage.removeItem('token');
+
+      setLogIn(false)
+      // Redirigir al usuario a la página de inicio de sesión
+      navigate('/login');
+   };
+
+   return (
+      <div className='w-full flex items-center justify-between h-[100px] bg-[#D9DADF text-white'>
+         <div className='flex items-center gap-4'>
+            <img src={logo} className='w-[60px] h-auto ml-10' alt="" />
+            <h1 className='text-black font-bold text-lg'>Inventario de bodegas</h1>
+         </div>
+         <div className='flex items-center gap-4 mr-10'>
+            <div className='text-black flex items-center gap-2'>
+               <FaUserCircle className='text-[38px] cursor-pointer' />
+               <h1 className='cursor-pointer font-bold text-[16px]'>Dorelia</h1>
+            </div>
+            <FaBell className='cursor-pointer text-black text-[25px]' />
+            <IoMdLogOut className='cursor-pointer text-black text-[30px] font-bold' onClick={handleLogout}/>
+         </div>
       </div>
-      <div className='flex items-center gap-4 mr-10'>
-        <div className='text-black flex items-center gap-2'>
-            <FaUserCircle  className='text-[38px] cursor-pointer' />
-            <h1 className='cursor-pointer font-bold text-[16px]'>Dorelia</h1>
-        </div>
-        <FaBell className='cursor-pointer text-black text-[25px]'/>
-        <MdKeyboardArrowDown className='cursor-pointer text-black text-[30px] font-bold' />
-      </div>
-    </div>
-  )
+   )
 }
