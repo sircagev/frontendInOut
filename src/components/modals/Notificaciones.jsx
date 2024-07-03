0566c43c1e21167464dc4d19f50f16eb814875f4
YberB <ybercoronell@gmail.com>
2024-07-03 14:58:36 -0500

    nabvar notificaciones

--- src/components/modals/Notificaciones.jsx
+++ src/components/modals/Notificaciones.jsx
@@ -6,6 +6,9 @@ const NotificationsModal = ({
   setShowModal,
   elementosConBajoStock,
   prestamosActivos,
+  elementosExpirados,
+  prestamosVencidos,
+  solicitudes,
 }) => {
   const navigate = useNavigate();
 
@@ -19,8 +22,23 @@ const NotificationsModal = ({
     navigate("/reportes/prestamosactivos");
   };
 
+  const handleViewVencidosClick = () => {
+    setShowModal(false);
+    navigate("/reportes/prestamosvencidos");
+  };
+
+  const handleViewSolicitudesClick = () => {
+    setShowModal(false);
+    navigate("/reportes/solicitud");
+  };
+
+  const handleViewExpiradosClick = () => {
+    setShowModal(false);
+    navigate("/reportes/elementosexpirados");
+  };
+
   return (
-    showModal && (elementosConBajoStock > 0 || prestamosActivos > 0) && (
+    showModal && (elementosConBajoStock > 0 || prestamosActivos > 0 || prestamosVencidos > 0 || solicitudes > 0 || elementosExpirados > 0) && (
       <div className="absolute top-7 right-8 z-50">
         <div
           className="fixed top-9 right-11 bottom-0 bg-gray-500 bg-opacity-50 cursor-pointer"
@@ -62,7 +80,49 @@ const NotificationsModal = ({
               <FaSearch className="text-blue-900" />
             </div>
           )}
-          <div className="flex justify-center mt-4">
+          {prestamosVencidos > 0 && (
+            <div
+              className="flex items-center hover:bg-gray-200 p-2 rounded cursor-pointer"
+              onClick={handleViewVencidosClick}
+            >
+              <p className="flex-1 text-black">
+                <span className="text-blue-700 font-bold">
+                  {prestamosVencidos}
+                </span>{" "}
+                Préstamos vencidos
+              </p>
+              <FaSearch className="text-blue-900" />
+            </div>
+          )}
+          {solicitudes > 0 && (
+            <div
+              className="flex items-center hover:bg-gray-200 p-2 rounded cursor-pointer"
+              onClick={handleViewSolicitudesClick}
+            >
+              <p className="flex-1 text-black">
+                <span className="text-blue-700 font-bold">
+                  {solicitudes}
+                </span>{" "}
+                Solicitudes
+              </p>
+              <FaSearch className="text-blue-900" />
+            </div>
+          )}
+          {elementosExpirados > 0 && (
+            <div
+              className="flex items-center hover:bg-gray-200 p-2 rounded cursor-pointer"
+              onClick={handleViewExpiradosClick}
+            >
+              <p className="flex-1 text-black">
+                <span className="text-blue-700 font-bold">
+                  {elementosExpirados}
+                </span>{" "}
+                Elementos expirados
+              </p>
+              <FaSearch className="text-blue-900" />
+            </div>
+          )}
+            <div className="flex justify-center mt-4">
             <button
               className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded ml-4"
               onClick={() => setShowModal(false)}
