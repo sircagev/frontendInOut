870afc926268a6c67ef1a536c6db6e3732b54c5f
cervantesjuand <juancervantes.1389@gmail.com>
2024-07-15 15:20:29 -0500

    cambios

--- src/components/Buttons/ButtonRegistrar.jsx
+++ src/components/Buttons/ButtonRegistrar.jsx
@@ -5,7 +5,7 @@ export const ButtonRegistrar = ({label}) => {
   return (
     <div>
         <Button className='cursor-pointer' color="success" type="submit">
-                <label className='font-bold text-white'>{label}</label>
+                <label className='font-bold text-white cursor-pointer'>{label}</label>
         </Button>
     </div>
   )
